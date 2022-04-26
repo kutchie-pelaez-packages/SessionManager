228f69d7647c8cab5ec522cad7538339f3ee403a
ivan <kulikovia.72@icloud.com>
2022-04-27 02:23:05 +0300

    add session property

--- Sources/SessionManager/SessionManager.swift
+++ Sources/SessionManager/SessionManager.swift
@@ -4,3 +4,7 @@ import Tweak
 public protocol SessionManager: TweakReceiver {
     var sessionValueSubject: ValueSubject<Int> { get }
 }
+
+extension SessionManager {
+    public var session: Int { sessionValueSubject.value }
+}
